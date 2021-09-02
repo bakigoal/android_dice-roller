d99ac1cb058f079b183af7668d84da9453467fa8
Бакиров Ильмир <ibakirov@cinimex.ru>
2021-09-02 19:17:46 +0300

    activity lifecycle

--- app/src/main/java/com/bakigoal/diceroller/MainActivity.kt
+++ app/src/main/java/com/bakigoal/diceroller/MainActivity.kt
@@ -2,10 +2,50 @@ package com.bakigoal.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("main", "on create")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("main", "on start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("main", "on resume")
+    }
+
+    /**
+     * Foreground lifetime:
+     * onResume -> onPause
+     */
+    override fun onPause() {
+        super.onPause()
+        Log.i("main", "on pause")
+    }
+
+    /**
+     * Visible lifetime:
+     * onStart -> onResume -> onPause -> onStop
+     */
+    override fun onStop() {
+        super.onStop()
+        Log.i("main", "on stop")
+    }
+
+    /**
+     * Entire lifetime:
+     * onCreate -> onStart -> onResume -> onPause -> onStop -> onDestroy
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("main", "on destroy")
     }
 }
