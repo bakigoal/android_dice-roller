d1111470408ad7bbf12a1c49d886ce876352fa82
Бакиров Ильмир <ibakirov@cinimex.ru>
2021-09-05 18:43:14 +0300

    add button and use it in activity

--- app/src/main/java/com/bakigoal/diceroller/MainActivity.kt
+++ app/src/main/java/com/bakigoal/diceroller/MainActivity.kt
@@ -3,13 +3,20 @@ package com.bakigoal.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         Log.i("main", "on create")
+
+        setContentView(R.layout.activity_main)
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStart() {
