2a61c059c3d0c615e4e7fb3b3498efc307ddbaf9
Бакиров Ильмир <ibakirov@cinimex.ru>
2021-09-05 19:00:06 +0300

    add dice images

--- app/src/main/java/com/bakigoal/diceroller/MainActivity.kt
+++ app/src/main/java/com/bakigoal/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bakigoal.diceroller
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
@@ -20,8 +21,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText = findViewById<TextView>(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage = findViewById<ImageView>(R.id.dice_image)
+        diceImage.setImageResource(getDiceImage(randomInt))
+    }
+
+    private fun getDiceImage(i: Int): Int = when (i) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
 
     override fun onStart() {
